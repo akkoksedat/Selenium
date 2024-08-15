11cddac8436c207e4076c99824796698b6e741ff
sedat <akkokksedat@gmail.com>
2024-08-15 16:44:23 +0300

    odev

--- src/test/java/org/example/seleniumTests.java
+++ src/test/java/org/example/seleniumTests.java
@@ -7,15 +7,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-
-import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class SeleniumTests {
+public class seleniumTests {
     private static WebDriver driver;
 
     @BeforeAll
@@ -29,9 +25,8 @@ public class SeleniumTests {
         WebElement buttonsMenu = driver.findElement(By.cssSelector("li#item-4"));
         buttonsMenu.click();
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
-        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#Orhsl")));
-        element.click();
+        WebElement clickbutton = driver.findElement(By.cssSelector("#Orhsl"));
+        clickbutton.click();
 
         WebElement message = driver.findElement(By.id("dynamicClickMessage"));
         assertEquals("You have done a dynamic click", message.getText(), "Mesaj doğrulandı.");
@@ -45,13 +40,13 @@ public class SeleniumTests {
         addButton.click();
 
         WebElement firstNameInput = driver.findElement(By.cssSelector("input#firstName"));
-        firstNameInput.sendKeys("John");
+        firstNameInput.sendKeys("sedat");
 
         WebElement lastNameInput = driver.findElement(By.cssSelector("input#lastName"));
-        lastNameInput.sendKeys("Doe");
+        lastNameInput.sendKeys("akkok");
 
         WebElement emailInput = driver.findElement(By.cssSelector("input#userEmail"));
-        emailInput.sendKeys("john.doe@example.com");
+        emailInput.sendKeys("sedatakkok@outlook.com");
 
         WebElement ageInput = driver.findElement(By.cssSelector("input#age"));
         ageInput.sendKeys("30");
@@ -60,10 +55,30 @@ public class SeleniumTests {
         salaryInput.sendKeys("50000");
 
         WebElement departmentInput = driver.findElement(By.cssSelector("input#department"));
-        departmentInput.sendKeys("Engineering");
+        departmentInput.sendKeys("qa engineer");
 
         WebElement submitButton = driver.findElement(By.id("submit"));
-        submitButton.click();  // Formu göndermek için bu satırı ekleyin
+        submitButton.click();
+
+
+        WebElement editButton = driver.findElement(By.cssSelector("span[title='Edit']"));
+        editButton.click();
+
+
+        WebElement firstNameEdit = driver.findElement(By.cssSelector("input#firstName"));
+        firstNameEdit.clear();
+        firstNameEdit.sendKeys("sedat2");
+
+        WebElement lastNameEdit = driver.findElement(By.cssSelector("input#lastName"));
+        lastNameEdit.clear();
+        lastNameEdit.sendKeys("akkok2");
+
+        WebElement salaryEdit = driver.findElement(By.cssSelector("input#salary"));
+        salaryEdit.clear();
+        salaryEdit.sendKeys("60000");
+
+        WebElement submitEditButton = driver.findElement(By.id("submit"));
+        submitEditButton.click();
     }
 
     @AfterAll
